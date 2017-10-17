30582f41c9e2c5e9bb16bad84d0a1a1a5844957b
Surbhit Rao <surbhitrao@gmail.com>
2017-10-17 14:56:26 +0530

    First commit

---
+++ app/src/main/java/com/android/nikichat/model/Group.java
@@ -0,0 +1,12 @@
+package com.android.nikichat.model;
+
+
+
+public class Group extends Room{
+    public String id;
+    public ListFriend listFriend;
+
+    public Group(){
+        listFriend = new ListFriend();
+    }
+}
---
+++ app/src/main/java/com/android/nikichat/model/Message.java
@@ -0,0 +1,10 @@
+package com.android.nikichat.model;
+
+
+
+public class Message{
+    public String idSender;
+    public String idReceiver;
+    public String text;
+    public long timestamp;
+}
